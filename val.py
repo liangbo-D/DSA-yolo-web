3d152c12db5cd80ab95701d69020f0c063047714
liangbo-D <210527416@qq.com>
2025-07-15 11:21:53 +0800

    predict.py

--- val.py
+++ val.py
@@ -1,17 +1,20 @@
-import warnings
-warnings.filterwarnings('ignore')
 from ultralytics import YOLO
+import sys
+import os
+
+def run_predict(input_path='input.jpg'):
+    model = YOLO('best.pt')  # 
+
+    results = model.predict(
+        source=input_path,  # 图像或视频路径
+        save=True,          # 保存带框结果图像
+        save_txt=True,      # 保存 txt 检测框
+        conf=0.5,           # 置信度阈值
+        imgsz=1024,          # 输入尺寸
+        device='cpu'        # Render 上默认用 CPU
+    )
+    return results
 
 if __name__ == '__main__':
-    model = YOLO('D:/python_program/yolov11/ultralytics-8.3.28/runs/detect/train20/weights/best.pt')
-    model.val(data='D:/python_program/yolov11/ultralytics-8.3.28/data.yaml',
-              imgsz=1024,
-              batch=16,
-              split='test',
-              workers=14,
-              device='0',
-              conf=0.5,  # 置信度阈值
-              save_json=True,
-              save_txt=True,
-              save_conf=True,
-              )
+    input_path = sys.argv[1] if len(sys.argv) > 1 else 'input.jpg'
+    run_predict(input_path)
