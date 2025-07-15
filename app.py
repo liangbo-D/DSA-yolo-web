b9d8d3a165f69b0dcca1d34c1a64c51d2153b450
liangbo-D <210527416@qq.com>
2025-07-15 17:37:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 from flask import Flask, request, jsonify
 from predict import run_predict
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
