23dc9b95dddd475e9a4510ae023f74ec0364a153
liangbo-D <210527416@qq.com>
2025-07-15 11:25:04 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,13 +1,20 @@
-import warnings
-
-warnings.filterwarnings('ignore')
 from ultralytics import YOLO
+import sys
+import os
+
+def run_predict(input_path='input.jpg'):
+    model = YOLO('best.pt')  # 模型路径相对或远程
+
+    results = model.predict(
+        source=input_path,  # 图像或视频路径
+        save=True,          # 保存带框结果图像
+        save_txt=True,      # 保存 txt 检测框
+        conf=0.5,           # 置信度阈值
+        imgsz=1024,          # 输入尺寸
+        device='cpu'        # Render 上默认用 CPU
+    )
+    return results
 
 if __name__ == '__main__':
-    model = YOLO('D:/python_program/yolov11/ultralytics-8.3.28/runs/detect/train9/weights/best.pt')
-    model.predict(source='D:/detectimages',
-                  imgsz=1024,
-                  conf=0.3,  # 置信度阈值
-                  device='0',
-                  save=True,
-                  )
+    input_path = sys.argv[1] if len(sys.argv) > 1 else 'input.jpg'
+    run_predict(input_path)
